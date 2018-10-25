837c990c5d62faed2b6617bf3bad9616b21aac2f
Fernando Portilla <fernandoportilla1@gmail.com>
2018-10-24 22:43:09 -0500

    route default list vehicles

--- apps/vehicles/urls.py
+++ apps/vehicles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from apps.vehicles.views import VehicleListView, VehicleCreateView
 
 urlpatterns = [
-    path('vehicle', VehicleListView.as_view(), name='vehicle-list-view'),
+    path('', VehicleListView.as_view(), name='vehicle-list-view'),
     path('vehicle/create', VehicleCreateView.as_view(), name='vehicle-create-view'),
 ]
